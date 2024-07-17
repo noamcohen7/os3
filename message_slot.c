88956a0667bd0da60f4eea827032f63affc87ee2
noamc <noam.cohen@safebreach.com>
2024-07-17 16:49:38 +0300

    Add print for the channel object in write

--- message_slot.c
+++ message_slot.c
@@ -167,7 +167,10 @@ static ssize_t device_write( struct file*       file,
     printk("Message wrote to channel: %d with minor: %d, length of message is: %d", channel->channel_id, iminor(file->f_inode), 
     channel->msg_length);
 
-    printk("Write message is: %s", channel->message);
+    printk("Write message is:\n");
+    for (i = 0; i < length; ++i) {
+        printk("%c", channel->message[i]);
+    }
     return i;
 }
 
